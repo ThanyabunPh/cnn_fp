da294c94ed029faab6f7f12c7be5590228940dc8
thanlnwza <thanyabun.ph@gmail.com>
2022-12-07 15:14:28 +0700

    Fix_bug

--- CalCconvLayer.py
+++ CalCconvLayer.py
@@ -5,18 +5,14 @@ pooling = 0
 
 class convnet:
     def __init__(self):
-        self.width = 32
-        self.height = 24
+        self.width = 28
+        self.height = 28
+        self.conv_layer = 0
 
-        self.input_channels = 1
+    def conv(self, kernel, stride, padding, size=None):
 
-
-    def conv(self, channels, kernel, stride, padding, size=None):
-
-        in_out_channels = [int(i) for i in channels]
-
-        global conv_layer
-        conv_layer += 1
+        # global conv_layer
+        self.conv_layer += 1
 
         self.kernel = [int(i) for i in kernel]
         self.width = size[0] if size else self.width
@@ -25,35 +21,47 @@ class convnet:
         self.stride = stride
         self.padding = padding
 
-
+        # 
         output_width = (round(self.width - self.kernel[0] + (2 * self.padding)) / self.stride) + 1
         output_height = (round(self.height - self.kernel[1] + (2 * self.padding)) / self.stride) + 1
 
-        print(f"Convolution Layer {conv_layer}\n")
-        print(f"input_channels: {self.input_channels} | output_channels: {self.output_channels} | kernel: {self.kernel} | stride: {self.stride} | padding: {self.padding}\n")
+        print(f"Convolution Layer {self.conv_layer}\n")
+        # print(f"input_channels: {self.input_channels} | output_channels: {self.output_channels} | kernel: {self.kernel} | stride: {self.stride} | padding: {self.padding}\n")
         print(f"output_width: ({self.width} - {self.kernel[0]} + (2 x {self.padding})) / {self.stride} + 1 = {round(output_width)}")
         print(f"output_height: ({self.height} - {self.kernel[1]} + (2 x {self.padding})) / {self.stride} + 1 = {round(output_height)}\n")
 
-        # conv_layer += 1
         return round(output_width), round(output_height)
 
-    def maxpool(self, maxpool_stride, maxpool_kernel, size):
+    def pooling(self, pooling_kernel, size, pooling_stride=0):
         output_width, output_height = size[0], size[1]
+        self.pooling_stride = pooling_stride
+        self.pooling_kernel = [int(i) for i in pooling_kernel]
 
-        self.maxpool_stride = maxpool_stride
-        self.maxpool_kernel = [int(i) for i in maxpool_kernel]
-
+        
         global pooling
         pooling += 1
 
-        pool_width = (round(output_width - self.maxpool_kernel[0]) / self.maxpool_stride) + 1
-        pool_height = (round(output_height - self.maxpool_kernel[1]) / self.maxpool_stride) + 1
+        if self.pooling_stride != 0:
+            print("Pooling stride is not 0")
+            pool_width = (round(output_width - self.pooling_kernel[0]) / self.pooling_stride) + 1
+            pool_height = (round(output_height - self.pooling_kernel[1]) / self.pooling_stride) + 1
+    
+            print(f"Pooling Layer {pooling}\n")
+            print(f"pool_width: (({output_width} - {self.pooling_kernel[0]}) / {self.pooling_stride}) + 1 = {round(pool_width)}")
+            print(f"pool_height: (({output_height} - {self.pooling_kernel[1]}) / {self.pooling_stride}) + 1 = {round(pool_height)}\n")
+    
+            return round(pool_width), round(pool_height)
 
-        print(f"Pooling Layer {pooling}\n")
-        print(f"pool_width: (({output_width} - {self.maxpool_kernel[0]}) / {self.maxpool_stride}) + 1 = {round(pool_width)}")
-        print(f"pool_height: (({output_height} - {self.maxpool_kernel[1]}) / {self.maxpool_stride}) + 1 = {round(pool_height)}\n")
+        else:
+            print("Pooling stride is 0")
+            pool_width = round(output_width / self.pooling_kernel[0])
+            pool_height = round(output_height / self.pooling_kernel[1])
+            print(f"Pooling Layer {pooling}\n")
+            print(f"pool_width: {output_width} / {self.pooling_kernel[0]} = {round(pool_width)}")
+            print(f"pool_height: {output_height} / {self.pooling_kernel[1]} = {round(pool_height)}\n")
+            return round(pool_width), round(pool_height)
 
-        return round(pool_width), round(pool_height), self.output_channels
+            
 
     def flatten(self, size, Classified):
         self.size = size
@@ -66,19 +74,11 @@ class convnet:
 def step():
         
     print("#" * 50)
-    conv1 = convnet().conv(channels=[1, 32], kernel=[4, 4], stride=1, padding=2)
-    pooling1 = convnet().maxpool(maxpool_stride=2, maxpool_kernel=[3, 3], size=conv1)
+    conv1 = convnet().conv(kernel=[5,5], stride=1, padding=0)
+    pooling1 = convnet().pooling(pooling_kernel=[2, 2], size=conv1, pooling_stride=1)
 
-
-    print("#" * 50)
-    conv2 = convnet().conv(kernel=[3, 3], stride=1, padding=2, size=pooling1)
-    pooling2 = convnet().maxpool(maxpool_stride=2, maxpool_kernel=[2, 2], size=conv2)
-
-    print("#" * 50)
-    conv3 = convnet().conv(kernel=[2, 2], stride=1, padding=1, size=pooling2)
-
-    print("#" * 50)
-    flat1 = convnet().flatten(size=conv3, Classified=4)
+    conv2 = convnet().conv(kernel=[5,5], stride=1, padding=0, size=pooling1)
+    pooling2 = convnet().pooling(pooling_kernel=[2, 2], size=conv2, pooling_stride=1)
 
 
 
